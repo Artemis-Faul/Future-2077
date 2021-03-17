0925fde65ad4d9d0071f206358007d44c6cce855
Artemis-Faul <elizabetbonnar@gmail.com>
2021-03-17 20:51:38 +0300

    менюшка для телефонов

---
+++ js/main_script.js
@@ -0,0 +1,36 @@
+var counter = 0;
+$(".left-menu").click(function(){
+	if(counter % 2 == 0){
+		$("#menuHand").css({"margin-top": "0vh"});
+		setTimeout(function(){
+			$("#menuHand li:nth-child(1) a").css({"margin-top": "0"})
+		}, 100);
+		setTimeout(function(){
+			$("#menuHand li:nth-child(2) a").css({"margin-top": "0"})
+		}, 200);
+		setTimeout(function(){
+			$("#menuHand li:nth-child(3) a").css({"margin-top": "0"})
+		}, 300);
+		setTimeout(function(){
+			$("#menuHand li:nth-child(4) a").css({"margin-top": "0"})
+		}, 400);
+		setTimeout(function(){
+			$("#menuHand li:nth-child(5) a").css({"margin-top": "0"})
+		}, 500);
+		$("#menuHand li a").css({"display": "block"});
+		$("#menuHand").css({"display": "flex"});
+		$("#lm1, #lm2, #lm3").css({"background": "#ed008c"});
+		$("#lm-title").css({"color": "#ed008c"});
+		
+	}else{
+		$("#menuHand").css({"margin-top": "-100vh"});
+		setTimeout(function(){
+			$("#menuHand li a").css({"margin-top": "60px"});
+			$("#lm1, #lm2, #lm3").css({"background": "#ffffff"});
+			$("#lm-title").css({"color": "#ffffff"});
+			$("#menuHand li a").css({"display": "none"});
+		}, 200);
+		
+	}
+	counter++;
+})
