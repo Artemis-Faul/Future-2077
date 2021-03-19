65bcad1cbfaefa3468a61fa41e5394056ad33e01
Artemis-Faul <75532723+Artemis-Faul@users.noreply.github.com>
2021-03-19 18:01:48 +0300

    Изменения в коде

--- js/main_script.js
+++ js/main_script.js
@@ -20,14 +20,14 @@ $(".left-menu").click(function(){
 		$("#menuHand li a").css({"display": "block"});
 		$("#menuHand").css({"display": "flex"});
 		$("#lm1, #lm2, #lm3").css({"background": "#ed008c"});
-		$("#lm-title").css({"color": "#ed008c"});
+		$(".lm-title").css({"color": "#ed008c"});
 		
 	}else{
 		$("#menuHand").css({"margin-top": "-100vh"});
 		setTimeout(function(){
 			$("#menuHand li a").css({"margin-top": "60px"});
 			$("#lm1, #lm2, #lm3").css({"background": "#ffffff"});
-			$("#lm-title").css({"color": "#ffffff"});
+			$(".lm-title").css({"color": "#ffffff"});
 			$("#menuHand li a").css({"display": "none"});
 		}, 200);
 		
