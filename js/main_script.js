e4ab1c320d23de0056c48c0564a758eb5768bcb0
Artemis-Faul <75532723+Artemis-Faul@users.noreply.github.com>
2021-03-20 23:30:57 +0300

    Поправка багов меню

--- js/main_script.js
+++ js/main_script.js
@@ -1,4 +1,6 @@
 var counter = 0;
+var scrollPos = 0;
+
 $(".left-menu").click(function(){
 	if(counter % 2 == 0){
 		$("#menuHand").css({"margin-top": "0vh"});
@@ -19,19 +21,32 @@ $(".left-menu").click(function(){
 		}, 500);
 		$("#menuHand li a").css({"display": "block"});
 		$("#menuHand").css({"display": "flex"});
-		$("#menuHand").css({"position": "fixed"});
+		$("#menuHand").css({"height": "100%"});
 		$("#lm1, #lm2, #lm3").css({"background": "#ed008c"});
 		$(".lm-title").css({"color": "#ed008c"});
-		
 	}else{
-		$("#menuHand").css({"margin-top": "-100vh"});
+		// $("#menuHand").css({"margin-top": "-100vh"});
 		setTimeout(function(){
+			$("#menuHand").css({"height": "0vh"});
 			$("#menuHand li a").css({"margin-top": "60px"});
 			$("#lm1, #lm2, #lm3").css({"background": "#ffffff"});
 			$(".lm-title").css({"color": "#ffffff"});
 			$("#menuHand li a").css({"display": "none"});
+			// $("#menuHand").css({"display": "none"});
 		}, 200);
 		
 	}
 	counter++;
 })
+
+$(window).scroll(function(){
+		   var st = $(this).scrollTop();
+		   if (st > scrollPos){
+		   	 $(".left-menu").css({"transition": "0.7s"});
+		     $(".left-menu").css({"margin-top": "18vh"});
+		   } else {
+		   	  $(".left-menu").css({"transition": "0.7s"});
+		      $(".left-menu").css({"margin-top": "1vh"});
+		   }
+		   scrollPos = st;
+		});
